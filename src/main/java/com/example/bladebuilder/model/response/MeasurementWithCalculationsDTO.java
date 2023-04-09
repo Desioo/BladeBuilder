c363481a1c043cfbc62bf1534640cd9c9aa2ec2a
Desioo <33566952+Desioo@users.noreply.github.com>
2023-04-10 00:12:34 +0200

    Usunięcie dziedziczenia ze wszystkich klas

--- src/main/java/com/example/bladebuilder/model/response/MeasurementWithCalculationsDTO.java
+++ src/main/java/com/example/bladebuilder/model/response/MeasurementWithCalculationsDTO.java
@@ -3,16 +3,7 @@ package com.example.bladebuilder.model.response;
 import com.example.bladebuilder.model.calculate.Knife;
 import com.example.bladebuilder.model.calculate.Separator;
 import com.example.bladebuilder.model.entity.Measurement;
-import com.example.bladebuilder.model.reguest.MeasurementRequestDTO;
 
-public class MeasurementWithCalculationsDTO extends Measurement {
 
-    private Separator separator;
-    private Knife knife;
-
-    public MeasurementWithCalculationsDTO(MeasurementRequestDTO measurementRequestDTO){
-        this.separator = new Separator(measurementRequestDTO);
-        this.knife = new Knife(measurementRequestDTO);
-    }
-
+public record MeasurementWithCalculationsDTO(Separator separator, Knife knife, Measurement measurement) {
 }
