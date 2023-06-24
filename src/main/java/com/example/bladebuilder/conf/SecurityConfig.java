c19d47d77c711ec27bc441ff88f6d806546b2b28
Desioo <33566952+Desioo@users.noreply.github.com>
2023-06-24 10:53:35 +0200

    Dodanie spring security

--- src/main/java/com/example/bladebuilder/conf/SecurityConfig.java
+++ src/main/java/com/example/bladebuilder/conf/SecurityConfig.java
@@ -1,15 +1,26 @@
 package com.example.bladebuilder.conf;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.stereotype.Controller;
 
 @Controller
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(requests -> requests
+                .requestMatchers("/**").permitAll());
+        return http.build();
     }
 
 }
