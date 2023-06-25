861244a3e7198ffbdec661a16e921442e41bd044
Desioo <33566952+Desioo@users.noreply.github.com>
2023-06-25 13:47:00 +0200

    Działająca autoryzacja - jednak z brzydkim kodem :P

--- src/main/java/com/example/bladebuilder/conf/SecurityConfig.java
+++ src/main/java/com/example/bladebuilder/conf/SecurityConfig.java
@@ -3,12 +3,16 @@ package com.example.bladebuilder.conf;
 import com.example.bladebuilder.model.entity.Role;
 import com.example.bladebuilder.model.entity.User;
 import com.example.bladebuilder.model.reguest.CustomUserDetails;
+import com.example.bladebuilder.service.UserService;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.security.core.Authentication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -24,11 +28,15 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 
 @Controller
 @EnableWebSecurity
-public class SecurityConfig{
+public class SecurityConfig {
+
+    @Autowired
+    private Environment environment;
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
@@ -46,32 +54,55 @@ public class SecurityConfig{
         return http.build();
     }
 
-    @Bean
+    @Autowired
     public Filter customFilter() {
         return new OncePerRequestFilter() {
             @Override
             protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-                Role role = new Role();
-                role.setName("ROLE_ADMIN");
+                String contentType = request.getContentType();
 
-                User user = new User();
-                user.setRoles(Set.of(role));
+                if (contentType != null && contentType.startsWith("application/json")) {
+                    // Odczytaj zawartość ciała żądania
+                    String requestBody = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
 
-                UserDetails userDetails = new CustomUserDetails(user);
+                    // Parsuj JSON do obiektu
+                    JsonNode jsonNode = new ObjectMapper().readTree(requestBody);
 
-                // Tworzymy Authentication na podstawie UserDetails
-                Authentication authentication = new UsernamePasswordAuthenticationToken(user, null, userDetails.getAuthorities());
+                    // Odczytaj parametry z JSON-a
+                    String adminName = jsonNode.get("adminName").asText();
+                    String adminPassword = jsonNode.get("adminPassword").asText();
 
-                // Ustawiamy kontekst bezpieczeństwa
-                SecurityContextHolder.getContext().setAuthentication(authentication);
+                    String configuredAdminName = environment.getProperty("admin.username");
+                    String configuredAdminPassword = environment.getProperty("admin.password");
 
-                // Kontynuujemy przetwarzanie żądania
-                filterChain.doFilter(request, response);
+                    User user = new User();
+                    Role role = new Role();
 
-                // Czyszczenie kontekstu bezpieczeństwa po przetworzeniu żądania
-                SecurityContextHolder.clearContext();
+                    if (configuredAdminName.equals(adminName) && configuredAdminPassword.equals(adminPassword)) {
+                        role.setName("ROLE_ADMIN");
+                    } else {
+                        role.setName("ROLE_USER");
+                    }
+
+                    user.setRoles(Set.of(role));
+
+                    UserDetails userDetails = new CustomUserDetails(user);
+
+                    // Tworzymy Authentication na podstawie UserDetails
+                    Authentication authentication = new UsernamePasswordAuthenticationToken(user, null, userDetails.getAuthorities());
+
+                    // Ustawiamy kontekst bezpieczeństwa
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+
+                    // Kontynuujemy przetwarzanie żądania
+                    filterChain.doFilter(request, response);
+
+                    // Czyszczenie kontekstu bezpieczeństwa po przetworzeniu żądania
+                    SecurityContextHolder.clearContext();
+                }
             }
+
         };
     }
 }
