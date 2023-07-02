e048c82ffb9586e9782074f1a1bf6c4cac71d71e
Grzegorz Popko <g.popko@o2.pl>
2023-07-02 17:22:09 +0200

    Nowy regex!!

--- src/main/java/com/example/bladebuilder/utils/RegexMapMatcher.java
+++ src/main/java/com/example/bladebuilder/utils/RegexMapMatcher.java
@@ -17,6 +17,12 @@ public class RegexMapMatcher {
                 "description", 4, "invoiceNumber", 5, "thickness", 6,
                 "width", 7, "location", 10, "ordersElementsLength", 11));
 
+        REGEX_ELEMENTS_INDEX_MAP.put("^\"(([\\d ]+\\\\t)((.+)\\\\t){3}\\d{4}-\\d{2}-\\d{2}\\\\t([\\d ]+\\\\t)"
+                        +"(((.+)\\\\t)){3}([\\d ,]+\\\\t){2}(((.+)\\\\t)){3}([A-Za-z\\d]{0,4})?\\n?)+\"$",
+                Map.of("idSeq", 0, "seq", 5, "customer", 6,
+                        "description", 8, "invoiceNumber", 7, "thickness", 9,
+                        "width", 10, "location", 14, "ordersElementsLength", 15));
+
     }
 
     public Map<String, Map<String, Integer>> getRegexELementsIndexMap() {
